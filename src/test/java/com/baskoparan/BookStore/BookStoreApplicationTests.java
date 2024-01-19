05f8e5becde1d2cea58f0d68732482241fa13f8b
Hilmi Baskoparan <h.baskoparan@gmail.com>
2024-01-20 01:06:14 +0300

    Initial commit

---
+++ src/test/java/com/baskoparan/BookStore/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baskoparan.BookStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
